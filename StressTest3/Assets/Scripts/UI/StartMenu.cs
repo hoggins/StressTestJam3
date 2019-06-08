37cc53f9470ac486d0d4cec0224334a33289223d
Mac Notebook <macnotebook@plarium.com>
2019-06-08 19:41:34 +0300

    Better start menu

--- StressTest3/Assets/Scripts/UI/StartMenu.cs
+++ StressTest3/Assets/Scripts/UI/StartMenu.cs
@@ -8,15 +8,21 @@ namespace UI
   public class StartMenu : MonoBehaviour
   {
     public Button PlayButton;
+    public Button CharacterButton;
+    public Button ExitButton;
 
     private void OnEnable()
     {
       PlayButton.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));
+      PlayButton.onClick.AddListener(() => SceneManager.LoadScene("CharacterScene"));
+      PlayButton.onClick.AddListener(() => Application.Quit());
     }
 
     private void OnDisable()
     {
       PlayButton.onClick.RemoveAllListeners();
+      CharacterButton.onClick.RemoveAllListeners();
+      ExitButton.onClick.RemoveAllListeners();
     }
   }
 }
